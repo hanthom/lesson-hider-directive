f61dc52d090cfbbdcae3c5d60adf8b41b704a61d
hanthom <m.hantho@gmail.com>
2015-11-10 15:28:08 -0600

    Updates mad for all steps except step 8

--- lessonHider.js
+++ lessonHider.js
@@ -1,7 +1,31 @@
 angular.module('directivePractice').directive('lessonHider', function() {
 
 	return {
-		templateUrl: "lessonHider.html",
+		templateUrl: "/lessonHider.html",
+		restrict: 'E',
+		scope: {
+			lesson: '=',
+			dayAlert: '&',
+		},
+		link: function(scopeDir, element, attrs) {
+			//console.log(scope, element, attrs);
+			scopeDir.getSchedule.then(function(res) {
+				scopeDir.schedule = res.data;
+
+				scopeDir.schedule.forEach(function(scheduleDay) {
+					if (scheduleDay.lesson === scopeDir.lesson) {
+						scopeDir.lesson = scopeDir.schedule.weekday;
+						element.css('text-decoration', 'underline');
+						return;
+					}
+				});
+			});
+
+
+		},
+		controller: function($scope, lessonService) {
+			$scope.getSchedule = lessonService.getSchedule();
+		}
 
 	};
 
