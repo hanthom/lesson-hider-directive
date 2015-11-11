40c205da2f37d98a8fafc076202dffe23e9eccdc
hanthom <m.hantho@gmail.com>
2015-11-11 10:16:59 -0600

    Update made for checkbox

--- lessonHider.js
+++ lessonHider.js
@@ -6,15 +6,22 @@ angular.module('directivePractice').directive('lessonHider', function() {
 		scope: {
 			lesson: '=',
 			dayAlert: '&',
+			line: '='
 		},
 		link: function(scopeDir, element, attrs) {
-			//console.log(scope, element, attrs);
+			console.log(scopeDir, element, attrs);
 			scopeDir.getSchedule.then(function(res) {
 				scopeDir.schedule = res.data;
+				var birdy = false;
+				scopeDir.$watch('line', function(newCheck, old) {
+					var decorator = newCheck && birdy ? 'line-through' : 'none';
+					element.css('text-decoration', decorator);
+				});
 
 				scopeDir.schedule.forEach(function(scheduleDay) {
 					if (scheduleDay.lesson === scopeDir.lesson) {
 						console.log(scheduleDay);
+						birdy = true;
 						scopeDir.lessonDay = scheduleDay.weekday;
 						element.css('text-decoration', 'line-through');
 						return;
